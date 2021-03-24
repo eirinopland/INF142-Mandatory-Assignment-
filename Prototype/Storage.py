38ea8c01e6e126c3da249dff72f0b0301f097f46
Sindre Larsen <MindreDisseHandling>
2021-03-24 18:44:06 +0100

    minor additions and fixes

--- Prototype/Storage.py
+++ Prototype/Storage.py
@@ -1,4 +1,3 @@
-
 import socket
 import json
 
@@ -25,72 +24,62 @@ class Storage:
         # might not be needed at all
         pass
 
-    # def receive_data_from_station_network(self):
-    #     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #create UDP socket
-    #     #TODO: not sure if we need sock.bind(('ip', 5555))
-    #     address = ('127.0.0.1', 5555)
-    #     sock.settimeout(1.0) #set timeout value of 1 second
-    #
-    #     for station in self.weather_stations:
-    #         sock.sendto(json.dumps({'command':1}), address) #sending request to the weatheer station for command
-    #         #TODO: make a protocol, decide port, commands, json etc.
-    #         #if data is received back from server, then print
-    #         try:
-    #             data = sock.recvfrom(1024)
-    #             j_data = json.loads(data)
-    #             temperature, percipitation = j_data['temperature'], j_data['percipitation'] #get temperature and percipitation values from data
-    #             print("Storage server #" + str(self.server_id) + " weather-station #" + str(
-    #             station.station_id) + " in location: " + station.location_name)
-    #             print(temperature)
-    #             print(percipitation)
-    #         #if data is not received back from server, time out
-    #         except socket.timeout:
-    #             print('Request timed out')
-    #
-    #         #temperature, percipitation = station.get_data_over_network()
-    #         #print("Storage server #" + str(self.server_id) + " weather-station #" + str(
-    #          #   station.station_id) + " in location: " + station.location_name)
-    #         #print(temperature)
-    #         #print(percipitation)
+    def receive_data_from_station_network(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #create UDP socket
+        #TODO: not sure if we need sock.bind(('ip', 5555))
+        address = ('127.0.0.1', 5555)
+        sock.settimeout(1.0) #set timeout value of 1 second
+
+        for station in self.weather_stations:
+            sock.sendto(json.dumps({'command':1}), address) #sending request to the weatheer station for command
+            #TODO: make a protocol, decide port, commands, json etc.
+            #if data is received back from server, then print
+            try:
+                data = sock.recvfrom(1024)
+                j_data = json.loads(data)
+                temperature, percipitation = j_data['temperature'], j_data['percipitation'] #get temperature and percipitation values from data
+                print("Storage server #" + str(self.server_id) + " weather-station #" + str(
+                station.station_id) + " in location: " + station.location_name)
+                print(temperature)
+                print(percipitation)
+            #if data is not received back from server, time out
+            except socket.timeout:
+                print('Request timed out')
 
     def receive_data_from_station_offline(self):
         for station in self.weather_stations:
             temperature, precipitation = station.get_data_offline()
-            # print("Storage server #" + str(self.server_id) + " weather-station #" + str(
-            #     station.station_id) + " in location: " + station.location_name)
-            # print(temperature)
-            # print(percipitation)
-            self.store_data_in_db(station,temperature,precipitation)
+            self.store_data_in_db(station, temperature, precipitation)
 
     def store_data_in_db(self, weather_station, temperature, precipitation):
-
         # login details for cluster:
-        # TODO Might have separate details for each server/storage?
+        # TODO Might have separate login or cluster_name details for each server/storage?
         password = "9FcPzJY7ogaHMn8d"
         username = "serverDB"
-        clusterName = "cluster0"
+        cluster_name = "cluster0"
         # Connect to cluster
-        client = MongoClient('mongodb+srv://' + username + ':' + password + '@' + clusterName
+        client = MongoClient('mongodb+srv://' + username + ':' + password + '@' + cluster_name
                              + '.hjee9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
-        # TODO accept weather data as arg instead of hardcoded here:
-        newWeatherData = {
+        new_weather_data = {
+            "Weather station ID": weather_station.station_id,
             "Location": weather_station.location_name,
             "temperature": temperature,
             "precipitation": precipitation
         }
-
         # Create a new database in your cluster
         database = client.Storage_server_test
-
         # Create a new collection in you database
-        Weather_station = database.Weather_station_test
-
-        Weather_station.insert_one(newWeatherData)
-        pass
+        weather_station = database.Weather_station_test
+        weather_station.insert_one(new_weather_data)
 
     def retrieve_data_from_db(self):
-        pass
-
-    def send_db_data(self):
+        # login details for cluster:
+        # TODO Might have separate login or cluster_name details for each server/storage?
+        password = "9FcPzJY7ogaHMn8d"
+        username = "serverDB"
+        cluster_name = "cluster0"
+        # Connect to cluster
+        client = MongoClient('mongodb+srv://' + username + ':' + password + '@' + cluster_name
+                             + '.hjee9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
         pass
